be7c833ccd32bf2ed3ae306b7d5132e6272c0af0
bruno13palhano <bruno13palhano@gmail.com>
2023-07-23 01:52:36 -0300

    Add new property and doc

--- shared/src/commonMain/kotlin/com/example/shopdanisms/entity/Product.kt
+++ shared/src/commonMain/kotlin/com/example/shopdanisms/entity/Product.kt
@@ -1,7 +1,25 @@
 package com.example.shopdanisms.entity
 
+/**
+ * Product as an Entity.
+ *
+ * An entity class to persist products in database.
+ * @property id product's id.
+ * @property name product's name.
+ * @property description product's description.
+ * @property company the company that produces the product.
+ * @property purchasePrice product purchase price.
+ * @property salePrice product sale price.
+ * @property isPaid defines if the product is paid.
+ * @property isSold defines if the product is sold.
+ */
 data class Product(
     val id: Long,
     val name: String,
-    val price: Float
+    val description: String,
+    val company: String,
+    val purchasePrice: Float,
+    val salePrice: Float,
+    val isPaid: Boolean,
+    val isSold: Boolean
 )
