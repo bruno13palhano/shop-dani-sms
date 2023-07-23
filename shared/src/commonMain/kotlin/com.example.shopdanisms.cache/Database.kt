5bce605ebf8487544240ac3707537c1da31beb25
bruno13palhano <bruno13palhano@gmail.com>
2023-07-23 01:54:59 -0300

    Implement CommonData interface

--- shared/src/commonMain/kotlin/com.example.shopdanisms.cache/Database.kt
+++ shared/src/commonMain/kotlin/com.example.shopdanisms.cache/Database.kt
@@ -3,39 +3,87 @@ package com.example.shopdanisms.cache
 import com.example.shopdanisms.entity.Product
 import com.squareup.sqldelight.runtime.coroutines.asFlow
 import com.squareup.sqldelight.runtime.coroutines.mapToList
+import com.squareup.sqldelight.runtime.coroutines.mapToOne
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 
-internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
+internal class Database(
+    databaseDriverFactory: DatabaseDriverFactory
+) : CommonData<Product> {
     private val database = AppDatabase(databaseDriverFactory.createDriver())
     private val dbQuery = database.appDatabaseQueries
 
-    internal fun getAll(): Flow<List<Product>> {
-        return dbQuery.getAll(::mapProduct).asFlow().mapToList(Dispatchers.IO)
+    override suspend fun insert(model: Product) {
+        dbQuery.insert(
+            name = model.name,
+            description = model.description,
+            company = model.company,
+            purchasePrice = model.purchasePrice.toDouble(),
+            salePrice = model.salePrice.toDouble(),
+            isPaid = model.isPaid,
+            isSold = model.isSold
+        )
     }
 
-    internal fun createProduct(product: Product) {
-        dbQuery.transaction {
-            dbQuery.insertProduct(
-                name = product.name,
-                price = product.price.toDouble()
-            )
-        }
+    override suspend fun update(model: Product) {
+        dbQuery.update(
+            id = model.id,
+            name = model.name,
+            description = model.description,
+            company = model.company,
+            purchasePrice = model.purchasePrice.toDouble(),
+            salePrice = model.salePrice.toDouble(),
+            isPaid = model.isPaid,
+            isSold = model.isSold
+        )
+    }
+
+    override suspend fun delete(id: Long) {
+        dbQuery.deleteById(id)
+    }
+
+    override fun getById(id: Long): Flow<Product> {
+        return dbQuery.getById(id = id, mapper = ::mapProduct)
+            .asFlow()
+            .map {
+                it.executeAsOne()
+            }
+            .catch { it.printStackTrace() }
+    }
+
+    override fun getAll(): Flow<List<Product>> {
+        return dbQuery.getAll(::mapProduct).asFlow().mapToList(Dispatchers.IO)
     }
 
     internal fun clearDatabase() {
         dbQuery.transaction {
-            dbQuery.removeAllProducts()
+            dbQuery.removeAll()
         }
     }
 
     private fun mapProduct(
         id: Long,
         name: String,
-        price: Double
+        description: String,
+        company: String,
+        purchasePrice: Double,
+        salePrice: Double,
+        isPaid: Boolean,
+        isSold: Boolean
     ): Product {
-        return Product(id, name, price.toFloat())
+        return Product(
+            id = id,
+            name = name,
+            description = description,
+            company = company,
+            purchasePrice = purchasePrice.toFloat(),
+            salePrice = salePrice.toFloat(),
+            isPaid = isPaid,
+            isSold = isSold
+        )
     }
 }
