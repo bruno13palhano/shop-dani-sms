328f6f82043c41e8d4ddde5d8aa4cfcc1ccf4b03
bruno13palhano <bruno13palhano@gmail.com>
2023-07-23 20:25:02 -0300

    add context

--- androidApp/src/main/java/com/example/shopdanisms/android/MainApplication.kt
+++ androidApp/src/main/java/com/example/shopdanisms/android/MainApplication.kt
@@ -10,7 +10,7 @@ class MainApplication : Application() {
         super.onCreate()
 
         startKoin {
-            modules(appModule() + androidModule)
+            modules(appModule() + androidModule(this@MainApplication))
         }
     }
 }
--- androidApp/src/main/java/di/AndroidModule.kt
+++ androidApp/src/main/java/di/AndroidModule.kt
@@ -1,9 +1,16 @@
 package di
 
+import android.content.Context
+import com.example.shopdanisms.SmsSDK
 import com.example.shopdanisms.android.GreetPresenter
+import com.example.shopdanisms.android.ui.viewmodel.MainActivityViewModel
+import com.example.shopdanisms.cache.DatabaseDriverFactory
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
-val androidModule = module {
+fun androidModule(context: Context) = module {
     singleOf(::GreetPresenter)
+    single { SmsSDK(DatabaseDriverFactory(context)) }
+    viewModel { MainActivityViewModel(get()) }
 }
