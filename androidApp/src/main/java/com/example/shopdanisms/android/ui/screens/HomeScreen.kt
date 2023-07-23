1acc658d7c43d3d9d798ca5cb3632ccd1580ca93
bruno13palhano <bruno13palhano@gmail.com>
2023-07-23 20:28:19 -0300

    Add HomeScreen

---
+++ androidApp/src/main/java/com/example/shopdanisms/android/ui/screens/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.shopdanisms.android.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home Screen")
+}
