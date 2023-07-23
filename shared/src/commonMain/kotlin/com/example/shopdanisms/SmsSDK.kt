a44e2be99a77f410840fbc66e89f66cbb9c22cd9
bruno13palhano <bruno13palhano@gmail.com>
2023-07-23 02:14:24 -0300

    SmsSDK implements CommonData interface

--- shared/src/commonMain/kotlin/com/example/shopdanisms/SmsSDK.kt
+++ shared/src/commonMain/kotlin/com/example/shopdanisms/SmsSDK.kt
@@ -1,18 +1,31 @@
 package com.example.shopdanisms
 
+import com.example.shopdanisms.cache.CommonData
 import com.example.shopdanisms.cache.Database
 import com.example.shopdanisms.cache.DatabaseDriverFactory
 import com.example.shopdanisms.entity.Product
 import kotlinx.coroutines.flow.Flow
 
-class SmsSDK(databaseDriverFactory: DatabaseDriverFactory) {
+class SmsSDK(databaseDriverFactory: DatabaseDriverFactory): CommonData<Product> {
     private val database = Database(databaseDriverFactory)
 
-    fun getProducts(): Flow<List<Product>> {
-        return database.getAll()
+    override suspend fun insert(model: Product) {
+        database.insert(model)
     }
 
-    fun insertProduct(product: Product) {
-        database.createProduct(product)
+    override suspend fun update(model: Product) {
+        database.update(model)
+    }
+
+    override suspend fun delete(id: Long) {
+        database.delete(id)
+    }
+
+    override fun getById(id: Long): Flow<Product> {
+        return database.getById(id)
+    }
+
+    override fun getAll(): Flow<List<Product>> {
+        return database.getAll()
     }
 }
